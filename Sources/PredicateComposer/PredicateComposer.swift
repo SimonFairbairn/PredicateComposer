59011cc0e1baea1290c502e2a49ddea3326beb17
Simon Fairbairn <simon@line-in.co.uk>
2022-02-15 21:56:41 -0700

    Adds new types and deprecates old enum cases

--- Sources/PredicateComposer/PredicateComposer.swift
+++ Sources/PredicateComposer/PredicateComposer.swift
@@ -14,21 +14,37 @@ public enum PredicateType {
 
 public enum Match {
 	case entity
+	@available(*, deprecated, message: "Use `relationshipWithEntityNamed` instead")
 	case entityNamed(String)
+	@available(*, deprecated, message: "Use `attribute` instead")
 	case entitiesWithAttribute(String)
+
+	case attribute(String)
+	case relationshipWithEntityNamed(String)
+	case entityRelationshipWithAttribute(NSManagedObject, String)
 }
 
-public enum SearchType : String {
+public enum SearchType: String {
 	case or
 	case and
 }
 
 public struct PredicateComposer {
-	let predicates : [SearchFor]
-	let combinedWith : SearchType
-	
-	public init( predicates : [SearchFor], combinedWith : SearchType = .or) {
+	let predicates: [SearchFor]
+	let combinedWith: SearchType
+
+	public init( predicates: [SearchFor], combinedWith: SearchType = .or) {
 		self.predicates = predicates
+		self.combinedWith = combinedWith
+	}
+}
+
+public struct SearchComposer {
+	let searches: [SearchFor]
+	let combinedWith: SearchType
+
+	public init( searches: [SearchFor], combinedWith: SearchType = .or) {
+		self.searches = searches
 		self.combinedWith = combinedWith
 	}
 }
@@ -37,13 +53,12 @@ public protocol PredicateComposing {
 	func requirements() -> PredicateComposer?
 }
 
-
 public struct SearchFor {
-	let attribute : String
-	let predicateType : PredicateType
-	let arguments : Any?
-	
-	public init( _ attribute : Match, that predicateType : PredicateType, arguments : Any? = nil) {
+	let attribute: String
+	let predicateType: PredicateType
+	let arguments: Any?
+
+	public init( _ attribute: Match, that predicateType: PredicateType, arguments: Any? = nil) {
 		switch attribute {
 		case .entity:
 			self.attribute = "self"
@@ -51,11 +66,20 @@ public struct SearchFor {
 			self.attribute = string
 		case .entitiesWithAttribute(let string):
 			self.attribute = string
+		case .attribute(let attribute):
+			self.attribute = attribute
+		case .relationshipWithEntityNamed(let entity):
+			self.attribute = entity
+		case .entityRelationshipWithAttribute(let obj, let attribute):
+			guard let name = obj.entity.name else {
+				fatalError("A NSManagedObject was passed to the SearchFor struct in Predicate Composer that does not have a valid Entity name")
+			}
+			self.attribute = "\(name).\(attribute)"
 		}
 		self.predicateType = predicateType
 		self.arguments = arguments
 	}
-	
+
 	internal func constructQuery() -> [(String, Any?)]? {
 		switch self.predicateType {
 		case .isTrue:
@@ -83,7 +107,7 @@ public struct SearchFor {
 			default:
 				return [("ANY \(attribute) IN %@", args)]
 			}
-			
+
 		case .haveAllOf(let argument):
 			guard let args = argument as? [Any] else {
 				return [("ANY \(attribute) == %@", argument)]
@@ -94,7 +118,7 @@ public struct SearchFor {
 			case 1:
 				return [("ANY \(attribute) IN %@", args)]
 			default:
-				var outStrings : [(String, Any?)] = []
+				var outStrings: [(String, Any?)] = []
 				for arg in args {
 					outStrings.append(("SUBQUERY(\(attribute), $att, $att == %@).@count == 1", arg))
 				}
@@ -104,22 +128,33 @@ public struct SearchFor {
 	}
 }
 
-public struct CoreDataPredicateComposer<T : NSManagedObject> {
-	
-	struct PredicateComposition {
-		let searchType : SearchType
-		var predicates : [SearchFor]
-	}
-	
-	private var composition : [PredicateComposition] = []
-	
-	public var predicate : NSPredicate? {
-		var predicateString : [String] = []
-		var argumentArray : [Any] = []
+public extension NSManagedObject {
 
-		var currentSearchType : SearchType = .and
+	static func generatePredicate( from searches: [SearchComposer] ) -> NSPredicate? {
+		var current = PredicateComposition(searchType: .and, predicates: [])
+
+		for req in searches {
+			if req.combinedWith != current.searchType {
+				current = PredicateComposition(searchType: req.combinedWith, predicates: [])
+			}
+			current.predicates.append(contentsOf: req.searches)
+		}
+
+		return self.predicate(with: [current] )
+	}
+
+	static func generatePredicate( _ search: SearchFor ) -> NSPredicate? {
+		let searchComposer = SearchComposer(searches: [search])
+		return Self.generatePredicate(from: [searchComposer])
+	}
+
+	static func predicate( with composition: [PredicateComposition]) -> NSPredicate? {
+		var predicateString: [String] = []
+		var argumentArray: [Any] = []
+
+		var currentSearchType: SearchType = .and
 		var finalPredicateString = ""
-		var predicateArray : [String] = []
+		var predicateArray: [String] = []
 		if !composition.isEmpty {
 			for comp in composition {
 				if comp.searchType != currentSearchType {
@@ -130,13 +165,13 @@ public struct CoreDataPredicateComposer<T : NSManagedObject> {
 					predicateString.removeAll()
 					currentSearchType = comp.searchType
 				}
-				
+
 				for req in comp.predicates {
 					guard let queries = req.constructQuery() else {
 						continue
 					}
-					var innerPredicateStrings : [String] = []
-					for string in queries  {
+					var innerPredicateStrings: [String] = []
+					for string in queries {
 						if let existentOptions = string.1 {
 							argumentArray.append(existentOptions)
 						}
@@ -145,35 +180,49 @@ public struct CoreDataPredicateComposer<T : NSManagedObject> {
 					if !innerPredicateStrings.isEmpty {
 						predicateString.append("(" + innerPredicateStrings.joined(separator: " AND ") + ")")
 					}
-					
+
 				}
 			}
 			finalPredicateString += predicateString.joined(separator: " \(currentSearchType.rawValue.uppercased()) ")
 			predicateArray.append(finalPredicateString)
 		}
-		
+
 		let validPredicates = predicateArray.filter({ !$0.isEmpty }).map({ "(\($0))"  }).joined(separator: " AND ")
-		
+
 		if !validPredicates.isEmpty {
 			return NSPredicate(format: validPredicates, argumentArray: argumentArray)
 		} else {
 			return nil
 		}
 	}
-	
-	
-	public init( requirements : [PredicateComposing] = []) {
-		
+
+}
+
+public	struct PredicateComposition {
+	let searchType: SearchType
+	var predicates: [SearchFor]
+}
+
+public struct CoreDataPredicateComposer<T: NSManagedObject> {
+
+	private var composition: [PredicateComposition] = []
+
+	public var predicate: NSPredicate? {
+		return T.predicate(with: composition)
+	}
+
+	public init( requirements: [PredicateComposing] = []) {
+
 		guard !requirements.isEmpty else {
 			return
 		}
 		var current = PredicateComposition(searchType: .and, predicates: [])
-		
+
 		for req in requirements {
 			guard let validReq = req.requirements() else {
 				continue
 			}
-			
+
 			if validReq.combinedWith != current.searchType {
 				self.composition.append(current)
 				current = PredicateComposition(searchType: validReq.combinedWith, predicates: [])
@@ -182,17 +231,17 @@ public struct CoreDataPredicateComposer<T : NSManagedObject> {
 		}
 		self.composition.append(current)
 	}
-	
-	mutating public func addPredicate( for attribute: Match, type : PredicateType, arguments : Any? = nil ) {
+
+	mutating public func addPredicate( for attribute: Match, type: PredicateType, arguments: Any? = nil ) {
 		let newPred = SearchFor(attribute, that: type)
 		self.composition.append(PredicateComposition(searchType: .or, predicates: [newPred]))
 	}
-	
-	mutating public func remove( _ requirement : PredicateComposing ) {
+
+	mutating public func remove( _ requirement: PredicateComposing ) {
 //		self.remove(requirement.requirements())
 	}
-	
-	mutating private func remove( _ predicates : [SearchFor] ) {
+
+	mutating private func remove( _ predicates: [SearchFor] ) {
 //		for predicate in predicates {
 //			guard let finalString = predicate.constructQuery() else {
 //				continue
@@ -207,28 +256,28 @@ public struct CoreDataPredicateComposer<T : NSManagedObject> {
 //			})
 //		}
 	}
-	
-	mutating public func add( _ requirement : PredicateComposing ) {
-		
+
+	mutating public func add( _ requirement: PredicateComposing ) {
+
 		guard let validReq = requirement.requirements() else {
 			return
 		}
-		
+
 		let comp = PredicateComposition(searchType: validReq.combinedWith, predicates: validReq.predicates)
 		self.composition.append(comp)
 
 	}
-	
-	public func dictionaryFetchRequest( entityName : String ) -> NSFetchRequest<NSFetchRequestResult> {
+
+	public func dictionaryFetchRequest( entityName: String ) -> NSFetchRequest<NSFetchRequestResult> {
 		let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: T.entity().name! )
 		fetchRequest.predicate = self.predicate
 		return fetchRequest
 	}
-	
+
 	public func fetchRequest() -> NSFetchRequest<T> {
 		let fetchRequest = NSFetchRequest<T>(entityName: T.entity().name! )
 		fetchRequest.predicate = self.predicate
 		return fetchRequest
 	}
-	
+
 }
--- Tests/PredicateComposerTests/PredicateComposerTests.swift
+++ Tests/PredicateComposerTests/PredicateComposerTests.swift
@@ -2,95 +2,93 @@ import XCTest
 import CoreData
 @testable import PredicateComposer
 
-
-enum NoteComposer : PredicateComposing {
+enum NoteComposer: PredicateComposing {
 	case searchString(String)
 	case exactMatch(Note)
 	case allMatching([Note])
 	case singleTag(Tag)
-	case tags([Tag],SearchType)
+	case tags([Tag], SearchType)
 	case tagsOrStringSearch([Tag], String, SearchType)
 	case alternativeSearch([String])
 	case beginsWith(String)
 	case allCompleted
 	case allNotCompleted
-	
-	private func searchPredicate(_ string : String ) -> SearchFor {
-		SearchFor(.entitiesWithAttribute("text"), that: .containsCaseInsensitive(string))
+
+	private func searchPredicate(_ string: String ) -> SearchFor {
+		SearchFor(.attribute("text"), that: .containsCaseInsensitive(string))
 	}
-	
+
 	func requirements() -> PredicateComposer? {
 		switch self {
 		case .searchString(let search):
-			return PredicateComposer(predicates: [searchPredicate(search)]) 
+			return PredicateComposer(predicates: [searchPredicate(search)])
 		case .exactMatch(let example):
-			return PredicateComposer(predicates:[SearchFor(.entity, that: .equals(example))])
+			return PredicateComposer(predicates: [SearchFor(.entity, that: .equals(example))])
 		case .allMatching(let notes):
-			return PredicateComposer(predicates:[SearchFor(.entity, that: .isInArray(notes))])
+			return PredicateComposer(predicates: [SearchFor(.entity, that: .isInArray(notes))])
 		case .singleTag(let tags):
-			return PredicateComposer(predicates:[SearchFor(.entitiesWithAttribute("tags"), that: .haveAllOf(tags))])
+			return PredicateComposer(predicates: [SearchFor(.attribute("tags"), that: .haveAllOf(tags))])
 		case .tags(let tags, let searchType):
 			switch searchType {
 			case .or:
-				return PredicateComposer(predicates:[
-					SearchFor(.entitiesWithAttribute("tags"), that: .haveAtLeastOneOf(tags))
+				return PredicateComposer(predicates: [
+					SearchFor(.attribute("tags"), that: .haveAtLeastOneOf(tags))
 				])
 			case .and:
-				return PredicateComposer(predicates:[
-					SearchFor(.entitiesWithAttribute("tags"), that: .haveAllOf(tags))
+				return PredicateComposer(predicates: [
+					SearchFor(.attribute("tags"), that: .haveAllOf(tags))
 				])
 			}
-			
+
 		case .tagsOrStringSearch(let tags, let searchString, let searchType):
 			switch searchType {
 			case .or:
-				return PredicateComposer(predicates:[
-					SearchFor(.entitiesWithAttribute("tags"), that: .haveAtLeastOneOf(tags)),
+				return PredicateComposer(predicates: [
+					SearchFor(.attribute("tags"), that: .haveAtLeastOneOf(tags)),
 					searchPredicate(searchString)
 				], combinedWith: .or)
 			case .and:
-				return PredicateComposer(predicates:[
-					SearchFor(.entitiesWithAttribute("tags"), that: .haveAllOf(tags)),
+				return PredicateComposer(predicates: [
+					SearchFor(.attribute("tags"), that: .haveAllOf(tags)),
 					searchPredicate(searchString)
 				], combinedWith: .or)
 			}
-			
+
 		case .alternativeSearch( let strings):
 			return PredicateComposer(predicates:
-										strings.map({ SearchFor(.entitiesWithAttribute("text"), that: .containsCaseInsensitive( $0 )) }),
+										strings.map({ SearchFor(.attribute("text"), that: .containsCaseInsensitive( $0 )) }),
 										   combinedWith: .or)
 		case .beginsWith( let string ):
-			return PredicateComposer(predicates: [SearchFor(.entitiesWithAttribute("text") , that: .beginsWithCaseInsensitive(string))])
+			return PredicateComposer(predicates: [SearchFor(.attribute("text"), that: .beginsWithCaseInsensitive(string))])
 		case .allCompleted:
-			return PredicateComposer(predicates: [SearchFor(.entitiesWithAttribute("isCompleted") , that: .isTrue)])
+			return PredicateComposer(predicates: [SearchFor(.attribute("isCompleted"), that: .isTrue)])
 		case .allNotCompleted:
-			return PredicateComposer(predicates: [SearchFor(.entitiesWithAttribute("isCompleted") , that: .isFalse)])
+			return PredicateComposer(predicates: [SearchFor(.attribute("isCompleted"), that: .isFalse)])
 		}
 	}
 }
 
-
 final class PredicateComposerTests: XCTestCase {
-	
+
 	static var model = CoreDataContainer()
-	
-	var exampleObjects : ( notes: [Note], tags: [Tag] )!
-		
+
+	var exampleObjects: ( notes: [Note], tags: [Tag] )!
+
 	override func setUp() {
 		self.exampleObjects = PredicateComposerTests.model.addExamples()
 		super.setUp()
 	}
-	
+
 	override func tearDown() {
 		PredicateComposerTests.model.remove(exampleObjects)
 		super.tearDown()
 	}
-	
+
     func test_PredicateComposer_stringSearchFortest_oneResult() {
-		
+
 		let predicate = CoreDataPredicateComposer<Note>(requirements: [NoteComposer.searchString( "test" )] )
 		let request = predicate.fetchRequest()
-	
+
 		do {
 			let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 			XCTAssertEqual(results.count, 1, "There should be exactly one result, \(results.count) found")
@@ -105,225 +103,225 @@ final class PredicateComposerTests: XCTestCase {
 
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
-		XCTAssertEqual( 0,results.count, "There should be exactly zero results, \(results.count) found")
+		XCTAssertEqual( 0, results.count, "There should be exactly zero results, \(results.count) found")
 	}
-	
+
 	func test_PredicateComposer_equalsCaseMatches_oneResult() throws {
 		var object = CoreDataPredicateComposer<Note>()
 		object.add(NoteComposer.exactMatch(exampleObjects.notes[1]))
-		
+
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: false)]
 
 		let string = exampleObjects.notes[1].text!
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
-		XCTAssertEqual( 1,results.count, "There should be exactly one results, \(results.count) found")
+		XCTAssertEqual( 1, results.count, "There should be exactly one results, \(results.count) found")
 		XCTAssertEqual(results.first?.text, string, "The retured results should have the string of the searched for object. Instead it has \(results.first?.text ?? "")")
 
 	}
-	
+
 	func test_PredicateComposer_arrayMatchLastTwoObjects_twoResults() throws {
-		var object = CoreDataPredicateComposer<Note>()
-		object.add(NoteComposer.allMatching( Array(exampleObjects.notes[1...2]) ))
-		
-		let request = object.fetchRequest()
+		let search1 = SearchComposer(searches: [
+			SearchFor(.entity, that: .isInArray(Array(exampleObjects.notes[1...2])))
+		])
+
+		let request = Note.fetchRequest()
+		request.predicate = Note.generatePredicate(from: [search1])
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
-		XCTAssertEqual( 2,results.count, "There should be exactly two results, \(results.count) found")
-		
+		XCTAssertEqual( 2, results.count, "There should be exactly two results, \(results.count) found")
+
 		try XCTSkipIf(results.count != 2)
-		
+
 		XCTAssertEqual(results[0], exampleObjects.notes[1], "The first result should equal the second object added to the database")
 		XCTAssertEqual(results[1], exampleObjects.notes[2], "The second result should equal the third object added to the database")
 
 	}
-	
+
 	func test_PredicateComposer_tag1Ortag2_twoResults() throws {
 		var object = CoreDataPredicateComposer<Note>()
 		object.add(NoteComposer.tags(exampleObjects.tags, .or))
-		
+
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
-		XCTAssertEqual( 3,results.count, "There should be exactly three results, \(results.count) found")
-		
+		XCTAssertEqual( 3, results.count, "There should be exactly three results, \(results.count) found")
+
 		try XCTSkipIf(results.count != 3)
-		
+
 		XCTAssertEqual(results[0], exampleObjects.notes[0], "The first result should equal the first object added to the database")
 		XCTAssertEqual(results[1], exampleObjects.notes[1], "The second result should equal the second object added to the database")
 		XCTAssertEqual(results[2], exampleObjects.notes[3], "The third result should equal the fourth object added to the database")
 
 	}
-	
+
 	func test_PredicateComposer_noTag_zeroResults() throws {
 		var object = CoreDataPredicateComposer<Note>()
 		object.add(NoteComposer.tags([], .or))
-		
+
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
-		XCTAssertEqual( 1,results.count, "There should be exactly one results, \(results.count) found")
-		
+		XCTAssertEqual( 1, results.count, "There should be exactly one results, \(results.count) found")
+
 		try XCTSkipIf(results.count != 1)
-		
+
 		XCTAssertEqual(results[0], exampleObjects.notes[2], "The first result should equal the third object added to the database")
-		
+
 	}
-	
-	
+
 	func test_PredicateComposer_lonelyTagOr_zeroResults() throws {
 		var object = CoreDataPredicateComposer<Note>()
 		object.add(NoteComposer.tags([exampleObjects.tags[2]], .or))
-		
+
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
-		XCTAssertEqual( 0,results.count, "There should be exactly zero results, \(results.count) found")
-		
+		XCTAssertEqual( 0, results.count, "There should be exactly zero results, \(results.count) found")
+
 	}
-	
+
 	func test_PredicateComposer_tag1AndTag2_oneResult() throws {
 		var object = CoreDataPredicateComposer<Note>()
 		object.add(NoteComposer.tags([exampleObjects.tags[0], exampleObjects.tags[1]], .and))
-		
+
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 		XCTAssertEqual(1, results.count, "There should be exactly one result, \(results.count) found")
-		
+
 		try XCTSkipIf(results.count != 1)
-		
+
 		XCTAssertEqual(results[0], exampleObjects.notes[0], "The first result should equal the first object added to the database")
 	}
-	
+
 	func test_PredicateComposer_tag1AndTag2OrStringMatch_twoResults() throws {
 		var object = CoreDataPredicateComposer<Note>()
 		object.add(NoteComposer.tagsOrStringSearch([exampleObjects.tags[0], exampleObjects.tags[1]], "without", .and))
-		
+
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 		XCTAssertEqual(2, results.count, "There should be exactly two results, \(results.count) found")
-		
+
 		try XCTSkipIf(results.count != 2)
-		
+
 		XCTAssertEqual(results[0], exampleObjects.notes[0], "The first result should equal the first object added to the database")
 		let text = try XCTUnwrap(results[1].text)
 		XCTAssert(text.contains("without"), "The second result should equal the third object added to the database: \(text)")
 	}
-	
+
 	func test_PredicateComposer_tag2_oneResult() throws {
 		var object = CoreDataPredicateComposer<Note>()
 		object.add(NoteComposer.tags([exampleObjects.tags[3]], .and))
-		
+
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 		XCTAssertEqual(1, results.count, "There should be exactly one result, \(results.count) found")
-		
+
 		try XCTSkipIf(results.count != 1)
-		
+
 		XCTAssertEqual(results[0], exampleObjects.notes[3], "The first result should equal the first object added to the database")
 
 	}
-	
-	
+
 	func test_PredicateComposer_tag1_twoResult() throws {
 		var object = CoreDataPredicateComposer<Note>()
 		object.add(NoteComposer.singleTag(exampleObjects.tags[0]))
-		
+
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 		XCTAssertEqual(2, results.count, "There should be exactly two results, \(results.count) found")
-		
+
 		try XCTSkipIf(results.count != 2)
-		
+
 		XCTAssertEqual(results[0], exampleObjects.notes[0], "The first result should equal the first object added to the database")
 		XCTAssertEqual(results[1], exampleObjects.notes[1], "The second result should equal the second object added to the database")
 	}
 
 	func test_PredicateComposer_note1AndNote3_twoResults() throws {
 		let object = CoreDataPredicateComposer<Note>(requirements: [NoteComposer.alternativeSearch(["test", "nothingburger"])])
-		
+
 		let request = object.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 		XCTAssertEqual(2, results.count, "There should be exactly two results, \(results.count) found")
-		
+
 		try XCTSkipIf(results.count != 2)
-		
+
 		XCTAssertEqual(results[0], exampleObjects.notes[0], "The first result should equal the first object added to the database")
 		XCTAssertEqual(results[1], exampleObjects.notes[1], "The second result should equal the second object added to the database")
 	}
-	
+
 	func test_PredicateComposer_emptyPredicate_noCrash() throws {
 		let predicate = CoreDataPredicateComposer<Note>()
-				
+
 		let request = predicate.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 		XCTAssertEqual(exampleObjects.notes.count, results.count, "There should be exactly the same number of results as notes, \(results.count) found")
 
 	}
-	
+
 	func test_PredicateComposer_beginsWith_() throws {
 		let predicate = CoreDataPredicateComposer<Note>(requirements: [NoteComposer.beginsWith("NOTHING")])
-		
+
 		let request = predicate.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 		XCTAssertEqual(1, results.count, "There should be exactly one note, \(results.count) found")
-		
+
 		try XCTSkipIf(results.count != 1)
-		
+
 		XCTAssertEqual(results[0].text, exampleObjects.notes[1].text, "The first result's text should equal the second object added to the database's text ('nothingburger')")
-		
+
 	}
-	
+
 	func test_PredicateComposer_isTrue_oneResult() throws {
 		let predicate = CoreDataPredicateComposer<Note>(requirements: [NoteComposer.allCompleted])
-		
+
 		let request = predicate.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 		XCTAssertEqual(1, results.count, "There should be exactly one note, \(results.count) found")
-		
+
 		try XCTSkipIf(results.count != 1)
-		
+
 		XCTAssertTrue(results[0].isCompleted)
-		
+
 	}
-	
+
 	func test_PredicateComposer_isFalse_threeResults() throws {
 		let predicate = CoreDataPredicateComposer<Note>(requirements: [NoteComposer.allNotCompleted])
-		
+
 		let request = predicate.fetchRequest()
 		request.sortDescriptors = [NSSortDescriptor(key: "added", ascending: true)]
-		
+
 		let results = try PredicateComposerTests.model.persistentContainer.viewContext.fetch(request)
 		XCTAssertEqual(3, results.count, "There should be three note, \(results.count) found")
-		
+
 		try XCTSkipIf(results.count != 3)
-		
+
 		XCTAssertFalse(results[0].isCompleted)
 		XCTAssertFalse(results[1].isCompleted)
 		XCTAssertFalse(results[2].isCompleted)
-		
+
 	}
 }
